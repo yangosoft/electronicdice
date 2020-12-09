587923467a6449d7a599bed276da83ec9d063e28
yangosoft <yangosoft@gmail.com>
2020-12-09 12:00:19 +0100

    More effects

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,6 @@
 
 #define LED_DDR DDRD
 #define LED_PORT PORTD
-void setupGPIO()
-{
-    DDRB = 0xFF; //1 output port
-    PORTB = 0xFF; // 1 high
-    
-    DDRD &= ~(1<<PD2);
-    PORTD |= (1<<PD2);
-    
-    
-    DDRB  = 0b00001000;   // PB3 output
-    DDRD  = 0b01100000;   // PD5 and PD6 outputs
-  
-    DDRD |= (1<<PD5); //R
-    DDRD |= (1<<PD6); //E
-    DDRD |= (1<<PD7); //A
-    DDRB |= (1<<PB0); //G
-    DDRB |= (1<<PB6);  
-    DDRB |= (1<<PB7);  //D
-}
 
 void setE(bool on)
 {
@@ -86,61 +67,10 @@ void setR(bool on)
 }
 
 
-void setRed(bool on)
-{
-    if(on)
-    {
-        //PORTD |= _BV(PD5);
-        //PORTD |= _BV(PD6);
-        //PORTD |= _BV(PD7);
-
-        //PORTB |= _BV(PB0);
-        //PORTB |= _BV(PB6);
-        PORTB |= _BV(PB7);
-
-    }else
-    {
-        //PORTD &= ~_BV(PD5);
-        //PORTD &= ~_BV(PD6);
-        //PORTD &= ~_BV(PD7);
-
-        //PORTB &= ~_BV(PB0);
-        //PORTB &= ~_BV(PB6);
-        PORTB &= ~_BV(PB7);
-
-    }
-}
-
-void setYellow(bool on)
-{
-    if(on)
-    {
-        PORTD |= _BV(PD7);
-    }else
-    {
-        PORTD &= ~_BV(PD7);
-    }
-}
-
-void setGreen(bool on)
-{
-    if(on)
-    {
-        PORTB |= _BV(PB0);
-    }else
-    {
-        PORTB &= ~_BV(PB0);
-    }
-}
-
-
 
 ISR (INT0_vect)
 {
       EIMSK = 0;
-      setRed(false);
-      setYellow(false);
-      setGreen(false);
 }
 
 
@@ -154,13 +84,7 @@ void disableInt0()
 
 void enableSleep()
 {
-    setRed(true);
-    setYellow(true);
-    setGreen(true);
-    _delay_ms(2000);
-    setRed(false);
-    setYellow(false);
-    setGreen(false);
+    
     
     
     cli();
@@ -179,31 +103,106 @@ void enableSleep()
         
 }
 
+void setupGPIO()
+{
+    DDRB = 0xFF; //1 output port
+    PORTB = 0xFF; // 1 high
+    
+    DDRD &= ~(1<<PD2);
+    PORTD |= (1<<PD2);
+    
+    
+    DDRB  = 0b00001000;   // PB3 output
+    DDRD  = 0b01100000;   // PD5 and PD6 outputs
+  
+    DDRD |= (1<<PD5); //R
+    DDRD |= (1<<PD6); //E
+    DDRD |= (1<<PD7); //A
+    DDRB |= (1<<PB0); //G
+    DDRB |= (1<<PB6);  
+    DDRB |= (1<<PB7);  //D
+    
+    setE(false);
+    setD(false);
+    setG(false);
+    setA(false);
+    setR(false);
+}
+
+void delay(int ms)
+{
+    while(ms > 0)
+    {
+        _delay_ms(1);
+        --ms;
+    }
+}
 
 void wakeUpSequence()
 {
-    for(int i = 0;i < 2; ++i)
+    for(int i = 0 ; i < 10; ++i)
     {
-        setYellow(false);
-        setGreen(false);
-        setRed(true);
-        _delay_ms(100);
-    
-        setRed(false);
-        setGreen(true);
-        _delay_ms(100);
-    
-        setYellow(true);
-        setGreen(false);
-        _delay_ms(100);
-        setYellow(false);
-        setRed(true);
-        _delay_ms(100);
+
+   setE(true);
+   _delay_ms(50);
+   setE(false);
+   setD(true);
+   _delay_ms(50);
+   setD(false);
+   setG(true);
+   _delay_ms(50);
+   setG(false);
+   setA(true);
+   _delay_ms(50);
+   setA(false);
+   setR(true);
+   _delay_ms(50);
+   setR(false);
+   setA(true);
+   _delay_ms(50);
+   setA(false);
+   setG(true);
+   _delay_ms(50);
+   setG(false);
+   setD(true);
+   _delay_ms(50);
+   setD(false);
+   setE(true);
+   _delay_ms(50);
+   setE(false);
     }
-    
-    setRed(false);
-    setYellow(false);
-    setGreen(false);
+
+    for(int i = 0; i < 75; i++)
+    {
+        setE(true);
+        setD(true);
+        setG(true);
+        setA(true);
+        setR(true);
+        delay(i);
+        setE(false);
+    setD(false);
+    setG(false);
+    setA(false);
+    setR(false);
+    delay(i);
+    }
+    for(int i = 75; i > 0; i--)
+    {
+        setE(true);
+        setD(true);
+        setG(true);
+        setA(true);
+        setR(true);
+        delay(i);
+        setE(false);
+    setD(false);
+    setG(false);
+    setA(false);
+    setR(false);
+    delay(i);
+    }
+
 }
 
 
@@ -212,16 +211,68 @@ void wakeUpSequence()
 int main (void) 
 {   
     setupGPIO();
-    setE(false);
-    setD(false);
-    setG(false);
-    setA(false);
-    setR(false);
+    wakeUpSequence();
+    
+    setE(true);
+    setD(true);
+    setG(true);
+    setA(true);
+    setR(true);
+    enableSleep();
+    /*OCR0A = 128;
+    // set PWM for 50% duty cycle
+
+
+    TCCR0A |= (1 << COM0A1);
+    // set none-inverting mode
+
+    TCCR0A |= (1 << WGM01) | (1 << WGM00);
+    // set fast PWM Mode
+
+    //TCCR0B |= (1 << CS01);
+    // set prescaler to 8 and starts PWM
+
+    TCCR0B |= (1 << CS02);
+    // set prescaler to 0 and starts PWM*/
+    int i = 5;
+    int j = 100;
+    bool increase = true;
     while(true)
     {
+        setE(true);
+        setD(true);
+        setG(true);
+        setA(true);
+        setR(true);
+        delay(i);
+        setE(false);
+        setD(false);
+        setG(false);
+        setA(false);
+        setR(false);
+        delay(i);
+        j--;
+        if(j==0)
+        {
+            if(false == increase)
+            {
+                i--;
+            }else
+            {
+                i++;
+            }
+            
+            j=100;
+        }
+        if((i<0) || (i>5))
+        {
+            increase = ~increase;
+        }
+
+       /* enableSleep();
       setR(true);
       _delay_ms(60);
       setR(false);
-      _delay_ms(400);
+      _delay_ms(400);*/
     }
 }
